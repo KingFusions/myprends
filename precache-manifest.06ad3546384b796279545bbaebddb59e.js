42ca1cec7ad14f5299ffd846316faff293297ccd
Stephen Bautista <spaulbautista@gmail.com>
2020-07-27 15:08:08 +0800

    Updates

---
+++ precache-manifest.06ad3546384b796279545bbaebddb59e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a56c8dff2cf2f6b99b125d32173e54b7",
+    "url": "/myprends/index.html"
+  },
+  {
+    "revision": "ec09d8af5384ccefa6e2",
+    "url": "/myprends/static/css/main.905d2e3a.chunk.css"
+  },
+  {
+    "revision": "1f61ebff6d85df8b6fef",
+    "url": "/myprends/static/js/2.ad85e31b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myprends/static/js/2.ad85e31b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec09d8af5384ccefa6e2",
+    "url": "/myprends/static/js/main.ef803f5c.chunk.js"
+  },
+  {
+    "revision": "57d9d546d10df6fe4a95",
+    "url": "/myprends/static/js/runtime-main.5b0ab78b.js"
+  }
+]);
